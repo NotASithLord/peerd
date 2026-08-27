b4268ef69304324e1b71c8908171092ee7b2372e
Ariel Deschapell <arieldeschapell@Ariels-MacBook-Air.local>
2026-08-27 21:01:34 +0800

    Fix restored host recovery boundaries

--- extension/background/vault-kernel.js
+++ extension/background/vault-kernel.js
@@ -854,7 +854,7 @@ const assemblyReport = () => Object.freeze({
       'runtime.onMessage': SEMANTIC_CUTOVER_SUMMARY.ready,
       'runtime.onConnect': true,
       'runtime.onInstalled': true,
-      'runtime.onUpdateAvailable': !!kernelUpdateCustody,
+      'runtime.onUpdateAvailable': kernelSelfHostedChrome && !!kernelUpdateCustody,
       'storage.session.onChanged': !!firefoxActorLifetime,
       'windows.onFocusChanged': true,
       'action.onClicked': true,
