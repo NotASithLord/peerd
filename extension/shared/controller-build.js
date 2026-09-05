95f6b35d51002da83ed3f13b269221e7282cc100
lordofthemodels <320223382+lordofthemodels@users.noreply.github.com>
2026-09-05 03:49:17 -0400

    Revert "Ship scanned PDF decoders"
    This reverts commit 165e36f07ca2a8d683748b31c0f2c655d7663beb.

--- extension/shared/controller-build.js
+++ extension/shared/controller-build.js
@@ -1,3 +1,3 @@
 // @ts-check
 // Cold identity leaf. Protocol and structured-clone validation stay demand-owned.
-export const CONTROLLER_BUILD_DIGEST = '34ee531ac7fef4c95df086d0362ffed710149ae5137a9d7299495addc24d460e';
+export const CONTROLLER_BUILD_DIGEST = 'afe656c91dea3dc0355b805666d660969d231d5a18e1e199bb70810325c180b2';
